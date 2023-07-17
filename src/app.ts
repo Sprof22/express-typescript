90354bd9691cc4d53d844d26a058944736a781f2
Sprof22 <richmondelaigwu@gmail.com>
2023-07-17 10:22:09 +0100

    typings update

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,46 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+interface Book {
+  title: string;
+  author: string;
+  pages: number;
+  rating: number;
+  genre: string;
+}
+
+function isValidBook(book: Book): boolean {
+  if (
+    book.title &&
+    book.author &&
+    typeof book.pages === "number" &&
+    typeof book.rating === "number" &&
+    book.genre &&
+    book.rating >= 0 &&
+    book.rating <= 10
+  ) {
+    return true;
+  }
+  return false;
+}
+
+const validateBookUpdates = (updates: Book) => {
+  if (!updates.title || !updates.author || !updates.pages || !updates.rating || !updates.genre) {
+    return "Please provide all the required fields: title, author, pages, rating, genre";
+  }
+
+  const rating = parseFloat(updates.rating.toString());
+  if (isNaN(rating) || rating < 0 || rating > 10) {
+    return "Rating must be a number between 0 and 10";
+  }
+  return null;
+};
+
+
+
+
+
 //data connection
 let db: any;
 connectToDb((err: any) => {
@@ -19,7 +59,7 @@ connectToDb((err: any) => {
   }
 });
 
-app.get("/books", (req, res) => {
+app.get("/books", (req: Request, res: Response) => {
   //current
 
   const page = Number(req.query.p) || 0;
@@ -39,7 +79,7 @@ app.get("/books", (req, res) => {
     });
 });
 
-app.get("/books/:id", (req, res) => {
+app.get("/books/:id", (req: Request, res: Response) => {
   if (ObjectId.isValid(req.params.id)) {
     db.collection("books")
       .findOne({ _id: new ObjectId(req.params.id) })
@@ -54,8 +94,12 @@ app.get("/books/:id", (req, res) => {
   }
 });
 
-app.post("/books", (req, res) => {
-  const book = req.body;
+app.post("/books", (req: Request, res: Response) => {
+  const book: Book = req.body;
+
+  if (!isValidBook(book)) {
+    return res.status(400).json({ error: "Invalid book object" });
+  }
 
   db.collection("books")
     .insertOne(book)
@@ -67,7 +111,7 @@ app.post("/books", (req, res) => {
     });
 });
 
-app.delete("/books/:id", (req, res) => {
+app.delete("/books/:id", (req: Request, res: Response) => {
   if (ObjectId.isValid(req.params.id)) {
     db.collection("books")
       .deleteOne({ _id: new ObjectId(req.params.id) })
@@ -82,7 +126,7 @@ app.delete("/books/:id", (req, res) => {
   }
 });
 
-app.patch("/books/:id", (req, res) => {
+app.patch("/books/:id", (req: Request, res: Response) => {
   const updates = req.body;
   if (ObjectId.isValid(req.params.id)) {
     db.collection("books")
@@ -99,8 +143,12 @@ app.patch("/books/:id", (req, res) => {
 });
 
 
-app.get("/pg/books", async (req, res) => {
+app.get("/pg/books", async (req: Request, res: Response) => {
+    const page = req.query.page ? parseInt(req.query.page as string) : 1;
+    const limit = req.query.limit ? parseInt(req.query.limit as string) : 10;
     try {
+        const offset = (page - 1) * limit;
+        const query = `SELECT * FROM books OFFSET ${offset} LIMIT ${limit}`;
       const results = await PostgresClient.query('SELECT * FROM books');
       return res.status(200).json(results.rows);
     } catch (error) {
@@ -109,7 +157,7 @@ app.get("/pg/books", async (req, res) => {
     }
   });
 
-  app.get("/pg/books/:id", async (req, res) => {
+  app.get("/pg/books/:id", async (req: Request, res: Response) => {
     const id = req.params.id;
     try {
       const { rows } = await PostgresClient.query(
@@ -132,7 +180,12 @@ app.get("/pg/books", async (req, res) => {
 
 
   app.post("/pg/books", async (req: Request, res: Response) => {
-    const book = req.body;
+    const book: Book = req.body;
+    
+    if (!isValidBook(book)) {
+      return res.status(400).json({ error: "Invalid book object" });
+    }
+
     try {
       const { rows } = await PostgresClient.query(
         "INSERT INTO books (title, author, pages, rating, genre) VALUES ($1, $2, $3, $4, $5) RETURNING *",
@@ -166,6 +219,13 @@ app.get("/pg/books", async (req, res) => {
   app.patch("/pg/books/:id", async (req: Request, res: Response) => {
     const { id } = req.params;
     const updates = req.body;
+    //validate the update
+    const validationError = validateBookUpdates(updates);
+    if (validationError) {
+      return res.status(400).json({ error: validationError });
+    }
+
+
     try {
       const { rowCount } = await PostgresClient.query(
         "UPDATE books SET title = $1, author = $2, pages = $3, rating = $4, genre = $5 WHERE id = $6",
